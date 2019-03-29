228684006b21115005001f89787fdd3a34a7b715
Jothi Pillay <jothipillay1996@gmail.com>
2019-03-30 01:06:03 +0800

    updated tests

--- src/test/java/CalculateOsaPointsTest.java
+++ src/test/java/CalculateOsaPointsTest.java
@@ -116,4 +116,156 @@ public class CalculateOsaPointsTest {
         student4.addToCcaList(cca6);
         assertEquals(62, student4.calculateOsaPoints());
     }
+
+    @Test
+    public void testChengWee() {
+        Student cw = new Student("1");
+        CCA cca1 = new CCA("Sports", 20); //tennis
+        CCA cca2 = new CCA("Admin", 17); //block comm
+        CCA cca3 = new CCA("Culture", 17); //choir
+        CCA cca4 = new CCA("Admin", 17); //rag
+        CCA cca5 = new CCA("Admin", 11); //kewoc ogl
+
+        cw.addToCcaList(cca1);
+        cw.addToCcaList(cca2);
+        cw.addToCcaList(cca3);
+        cw.addToCcaList(cca4);
+        cw.addToCcaList(cca5);
+
+        assertEquals(71, cw.calculateOsaPoints());
+    }
+
+    @Test
+    public void testJiaHua() {
+        Student jh = new Student("1");
+        CCA cca1 = new CCA("Sports", 9); //badminton
+        CCA cca2 = new CCA("Sports", 16); //squash
+        CCA cca3 = new CCA("Admin", 14); //laos
+        CCA cca4 = new CCA("Admin", 15); //convening
+
+        jh.addToCcaList(cca1);
+        jh.addToCcaList(cca2);
+        jh.addToCcaList(cca3);
+        jh.addToCcaList(cca4);
+
+        assertEquals(54, jh.calculateOsaPoints());
+
+    }
+
+    @Test
+    public void testZhenLun() {
+        Student zl = new Student("1");
+        CCA cca1 = new CCA("Sports", 16); //floorball
+        CCA cca2 = new CCA("Sports", 10); //tt
+        CCA cca3 = new CCA("Sports", 13); //sepak
+        CCA cca4 = new CCA("Sports", 9); //softball
+        CCA cca5 = new CCA("Sports", 2); //volleyball
+        CCA cca6 = new CCA("Sports", 2); //track
+        CCA cca7 = new CCA("Sports", 7); //handball
+        CCA cca8 = new CCA("Admin", 1); //SMC
+
+
+        zl.addToCcaList(cca1);
+        zl.addToCcaList(cca2);
+        zl.addToCcaList(cca3);
+        zl.addToCcaList(cca4);
+        zl.addToCcaList(cca5);
+        zl.addToCcaList(cca6);
+        zl.addToCcaList(cca7);
+        zl.addToCcaList(cca8);
+
+        assertEquals(40, zl.calculateOsaPoints());
+    }
+
+    @Test
+    public void testTobi() {
+        Student zl = new Student("1");
+        CCA cca1 = new CCA("Admin", 16); //rag
+        CCA cca2 = new CCA("Sports", 15); //swimming
+        CCA cca3 = new CCA("Sports", 12); //rr
+        CCA cca4 = new CCA("Admin", 17); //laos
+        CCA cca5 = new CCA("Admin", 7); //kewoc ogl
+
+        zl.addToCcaList(cca1);
+        zl.addToCcaList(cca2);
+        zl.addToCcaList(cca3);
+        zl.addToCcaList(cca4);
+        zl.addToCcaList(cca5);
+
+        assertEquals(60, zl.calculateOsaPoints());
+    }
+
+    @Test
+    public void testKim() {
+        Student zl = new Student("1");
+        CCA cca1 = new CCA("Admin", 16); //rag
+        CCA cca2 = new CCA("Sports", 8); //track
+        CCA cca3 = new CCA("Sports", 16); //rr
+        CCA cca4 = new CCA("Culture", 16); //dance
+
+        zl.addToCcaList(cca1);
+        zl.addToCcaList(cca2);
+        zl.addToCcaList(cca3);
+        zl.addToCcaList(cca4);
+
+        assertEquals(56, zl.calculateOsaPoints());
+
+    }
+
+    @Test
+    public void testKaiwen() {
+        Student zl = new Student("1");
+        CCA cca1 = new CCA("Admin", 14); //block comm
+        CCA cca2 = new CCA("Sports", 14); //frisbee
+        CCA cca3 = new CCA("Sports", 20); //rr
+        CCA cca4 = new CCA("Culture", 13); //dance
+        CCA cca5 = new CCA("Admin", 16); //convening
+        CCA cca6 = new CCA("Admin", 16); //ocip
+
+
+        zl.addToCcaList(cca1);
+        zl.addToCcaList(cca2);
+        zl.addToCcaList(cca3);
+        zl.addToCcaList(cca4);
+        zl.addToCcaList(cca5);
+        zl.addToCcaList(cca6);
+
+        assertEquals(66, zl.calculateOsaPoints());
+    }
+
+    @Test
+    public void testJieCong() {
+        Student zl = new Student("1");
+        CCA cca1 = new CCA("Admin", 17); //tdc
+        CCA cca2 = new CCA("Sports", 10); //trug
+        CCA cca3 = new CCA("Sports", 16); //vb
+        CCA cca4 = new CCA("Admin", 11); //smc
+        CCA cca5 = new CCA("Admin", 4); //convening
+
+        zl.addToCcaList(cca1);
+        zl.addToCcaList(cca2);
+        zl.addToCcaList(cca3);
+        zl.addToCcaList(cca4);
+        zl.addToCcaList(cca5);
+
+        assertEquals(54, zl.calculateOsaPoints());
+    }
+
+    @Test
+    public void testYekCheong() {
+        Student zl = new Student("1");
+        CCA cca1 = new CCA("Admin", 13); //block comm
+        CCA cca2 = new CCA("Admin", 14); //socs
+        CCA cca3 = new CCA("Sports", 18); //tennis
+        CCA cca4 = new CCA("Sports", 11); //rr
+        CCA cca5 = new CCA("Admin", 11); //rag
+
+        zl.addToCcaList(cca1);
+        zl.addToCcaList(cca2);
+        zl.addToCcaList(cca3);
+        zl.addToCcaList(cca4);
+        zl.addToCcaList(cca5);
+
+        assertEquals(56, zl.calculateOsaPoints());
+    }
 }
